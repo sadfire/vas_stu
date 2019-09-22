674d1c47c0c35317daa492cf7e7db87dc6cc3a02
arkhipowasilii <arkhipowasilii@gmail.com>
2019-09-22 10:26:37 +0300

    created function `merge`

--- sadnbox.py
+++ sadnbox.py
@@ -3,8 +3,30 @@ from typing import List, Tuple
 
 # [(1, 3), (2, 6), (8, 10), (15, 18)] -> [(1, 6), (8, 10), (15, 18)]
 def merge(intervals: List[Tuple[int, int]]) -> List[Tuple[int, int]]:
-    pass
+    new_intervals = []
+
+    left_num = None
+    right_num = None
+
+    for index in range(len(intervals) + 1):
+        try:
+            if right_num > intervals[index][0]:
+                right_num = intervals[index][1]
+
+            else:
+                new_intervals.append((left_num, right_num))
+                left_num = None
+                right_num = None
+
+        except:
+            if left_num and right_num is not None:
+                new_intervals.append((left_num, right_num))
+
+        left_num = intervals[index][0] if left_num is None else left_num
+        right_num = intervals[index][1] if right_num is None else right_num
+
+    return new_intervals
 
 
 if __name__ == '__main__':
-    pass
+    print(merge([(1, 3), (2, 6), (5, 10), (15, 18), (17, 25)]))
